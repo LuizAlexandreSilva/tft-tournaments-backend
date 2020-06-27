54bf01443edcd94e1f999ab249f561bb09b0dce5
LuizAlexandreSilva <alexandrehsilva96@gmail.com>
2020-06-27 00:39:00 -0400

    add request validator

--- src/modules/users/infra/http/routes/profile.routes.ts
+++ src/modules/users/infra/http/routes/profile.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import ProfileController from '@modules/users/infra/http/controllers/ProfileController';
+import { celebrate, Joi, Segments } from 'celebrate';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const profileRouter = Router();
@@ -9,6 +10,17 @@ const profileController = new ProfileController();
 profileRouter.use(ensureAuthenticated);
 
 profileRouter.get('/', profileController.show);
-profileRouter.put('/', profileController.update);
+profileRouter.put(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().required(),
+      password: Joi.string().required(),
+      newPassword: Joi.string(),
+      passwordConfirmation: Joi.string().valid(Joi.ref('newPassword')),
+    },
+  }),
+  profileController.update,
+);
 
 export default profileRouter;
